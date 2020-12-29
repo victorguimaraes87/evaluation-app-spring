0b7006ff118729eef4cbd741a02bd48864c9c2af
Victor Guimarães <victor.ti@abrhrs.org.br>
2020-12-29 19:46:59 -0300

    first commit

---
+++ src/main/java/app/victor/evaluation/EvaluationApplication.java
@@ -0,0 +1,13 @@
+package app.victor.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationApplication.class, args);
+	}
+
+}
